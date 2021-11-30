7ce437337292aed4956557bdbc97fbc797ea0c61
ZvonimirSkorin <80110264+ZvonimirSkorin@users.noreply.github.com>
2021-11-30 01:50:17 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 const conf = {
-  appId: "ac4e325a-6c4f-408f-b6a9-393f0d84d38e",
+  appId: "" ,
   redirectUri: "http://localhost:3000",
   scopes: ["user.read"],
-  authority: "https://login.microsoftonline.com/ff41217d-9ad8-48e2-a6f5-eac6b5de95a3",
+  authority: "https://login.microsoftonline.com",
 };
 
 module.exports = { conf };
